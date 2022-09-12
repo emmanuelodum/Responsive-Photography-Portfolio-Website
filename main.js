99fc7408611d0cff2d9772d653ce9d786b12fc6a
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-09-12 14:23:43 +0100

    Header styles

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').
         classList.toggle
-        ('window-scrolled'), window.
-        scrollY > 0
+        ('window-scrolled', window.
+            scrollY > 0);
 })
